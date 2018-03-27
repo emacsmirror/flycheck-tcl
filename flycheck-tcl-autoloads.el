ceaf31036fd06f81e61daab184cb704dc2f474ff
Niels Widger <niels.widger@gmail.com>
2018-03-27 08:27:06 -0400

    Create flycheck-tcl-autoloads.el

---
+++ flycheck-tcl-autoloads.el
@@ -0,0 +1,16 @@
+;;; flycheck-tcl-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("flycheck-tcl.el") (22915 14856 516834
+;;;;;;  719000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; flycheck-tcl-autoloads.el ends here
